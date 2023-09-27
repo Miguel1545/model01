04adbf92cb2cfedc36bf746216b776f003a5e0fe
Miguel1545 <1545anonimo@gmail.com>
2023-09-26 23:48:30 -0500

    mmmmm

--- js.js
+++ js.js
@@ -1,19 +1,79 @@
-
-document.getElementById('en').addEventListener('click', function() {
-    var textoEntrada = document.getElementById('entrada').innerText;
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=fr,en', true);
-    xhr.setRequestHeader('Ocp-Apim-Subscription-Key', '4cfa4d9c8be143b98089561423c86b7d');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Ocp-Apim-Subscription-Region', 'eastus')
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var jsonResponse = JSON.parse(xhr.responseText);
-            document.getElementById('salida').innerText = jsonResponse[0].translations[0,1].text;
-        }
-    };
-    xhr.send(JSON.stringify([{ Text: textoEntrada }]));
+document.getElementById('img').addEventListener('change', function() { 
+    var reader = new FileReader(); 
+    reader.onload = function(e) { 
+        var img = document.createElement('img'); 
+        img.className = ('imagen') 
+        img.src = e.target.result; 
+        document.getElementById('mostrar-img').appendChild(img); 
+    }
+    reader.readAsDataURL(this.files[0]); 
 });
 
+document.getElementById('limpiar').addEventListener('click', function() { 
+    document.getElementById('img').value = ''; 
+    document.getElementById('resultado-clasificacion').innerHTML = "";
+    var imgContainer = document.getElementById('mostrar-img');  
+    while (imgContainer.firstChild) {
+        imgContainer.removeChild(imgContainer.firstChild); 
+    } 
+    document.getElementById('results').value = "";
+});
 
+document.getElementById('estilo-boton').addEventListener('click', function() { 
+    var imgEntrada = document.getElementById('img');  
+    if (imgEntrada.files.length > 0) { 
+        var reader = new FileReader(); 
+        reader.onload = function(e) { 
+            var lecturaResul = reader.result; 
+            var api = new XMLHttpRequest(); 
+            api.open('POST', 'https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/edb69521-fde2-4272-9d75-9afb93eeb0ac/classify/iterations/modelo/image', true); 
+            api.setRequestHeader('Prediction-Key', '7575c83fb4fe49798d79be9ddeb66d54'); 
+            api.onreadystatechange = function () { 
+                if (api.readyState === 4 && api.status === 200) {  
+                    var respuestaJson = JSON.parse(api.responseText); 
+                    var prediccion = respuestaJson.predictions[0]; 
+                    var probabilidad = Math.round(prediccion.probability * 100); 
+                    if(probabilidad <= 60){
+                        alert("conflicto de deteccion") 
+                    }else{
+                        document.getElementById('resultado-clasificacion').innerText = 'Clase: ' + prediccion.tagName + ', Probabilidad: ' + probabilidad+ '%'; 
+                    }
+                    xhrTraduccion.send(JSON.stringify([{ 'Text': prediccion.tagName }]));
+                }
+                
+            };
+            api.send(lecturaResul); // Envio la solicitud a la API con los datos de la imagen
+        }
+        reader.readAsArrayBuffer(imgEntrada.files[0]); //Inicio la lectura del archivo de la imagen como un ArrayBuffer
+        
+        // Creo una nueva solicitud HTTP para hacer una petición a la API de traducción
+        var xhrTraduccion = new XMLHttpRequest();
+        xhrTraduccion.open('POST', 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en,fr,it,ko,ru', true);
+        xhrTraduccion.setRequestHeader('Ocp-Apim-Subscription-Key', '4cfa4d9c8be143b98089561423c86b7d');
+        xhrTraduccion.setRequestHeader('Ocp-Apim-Subscription-Region', 'eastus');
+        xhrTraduccion.setRequestHeader('Content-Type', 'application/json');
+        xhrTraduccion.onreadystatechange = function () {
+            // Verifico si la solicitud se completó y fue exitosa
+            if (xhrTraduccion.readyState === 4 && xhrTraduccion.status === 200) {
+                // Parseo la respuesta de la API a un objeto JSON
+                var respuestaTraduccion = JSON.parse(xhrTraduccion.responseText);
+                console.log(respuestaTraduccion)
+                // Creo una variable para almacenar los resultados de las traducciones
+                var resultadosTraducciones = '';
+
+                // Itero sobre cada traducción y agrego los resultados a la variable
+                for (var i = 0; i < respuestaTraduccion.length; i++) {
+                    for (var j = 0; j < respuestaTraduccion[i].translations.length; j++) {
+                        resultadosTraducciones += respuestaTraduccion[i].translations[j].to + ': ' + respuestaTraduccion[i].translations[j].text + '\n';
+                    }
+                }
+                // Muestro los resultados en el área de texto con id 'results'
+                document.getElementById('results').value = resultadosTraducciones;
+            }
+        };
+    
+
+    }
+
+});
 
--- scrip.js
+++ scrip.js
@@ -18,6 +18,7 @@ document.getElementById('limpiar').addEventListener('click', function() { //capt
     while (imgContainer.firstChild) {
         imgContainer.removeChild(imgContainer.firstChild); 
     } // verifico que el contenedor de la imagen tenga algun contenido y si es asi elimino el nodo hijo que tenia el contenedor de la imagen
+    document.getElementById('results').value = "";
 });
 
 // hago llamado a la API utilizando XMLHttpRequest y muestro los resultaos devueltos por esta
@@ -35,20 +36,43 @@ document.getElementById('estilo-boton').addEventListener('click', function() { /
                     var respuestaJson = JSON.parse(api.responseText); //guardo los resultados devueltos por la API 
                     var prediccion = respuestaJson.predictions[0]; // Extraigo las predicciones de la respuesta
                     var probabilidad = Math.round(prediccion.probability * 100); //redondeo la probabilidad al entero mas cercano
-                    document.getElementById('resultado-clasificacion').innerText = 'Clase: ' + prediccion.tagName + ', Probabilidad: ' + probabilidad+ '%'; //muestro los resultados en el contenedor con id resultado-clasificacion que es una etiqueta <p>
+                    if(probabilidad <= 60){
+                        alert("conflicto de deteccion") //si la probabilidad es menor o igual a 60 no detectara nada
+                    }else{
+                        document.getElementById('resultado-clasificacion').innerText = 'Clase: ' + prediccion.tagName + ', Probabilidad: ' + probabilidad+ '%'; //muestro los resultados en el contenedor con id resultado-clasificacion que es una etiqueta <p>
+                    }
+                    xhrTraduccion.send(JSON.stringify([{ 'Text': prediccion.tagName }]));
                 }
             };
             api.send(lecturaResul); // Envio la solicitud a la API con los datos de la imagen
         }
         reader.readAsArrayBuffer(imgEntrada.files[0]); //Inicio la lectura del archivo de la imagen como un ArrayBuffer
+
+        // Creo una nueva solicitud HTTP para hacer una petición a la API de traducción
+        var xhrTraduccion = new XMLHttpRequest();
+        xhrTraduccion.open('POST', 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en,fr,it,ko,ru', true);
+        xhrTraduccion.setRequestHeader('Ocp-Apim-Subscription-Key', '4cfa4d9c8be143b98089561423c86b7d');
+        xhrTraduccion.setRequestHeader('Ocp-Apim-Subscription-Region', 'eastus');
+        xhrTraduccion.setRequestHeader('Content-Type', 'application/json');
+        xhrTraduccion.onreadystatechange = function () {
+            // Verifico si la solicitud se completó y fue exitosa
+            if (xhrTraduccion.readyState === 4 && xhrTraduccion.status === 200) {
+                // Parseo la respuesta de la API a un objeto JSON
+                var respuestaTraduccion = JSON.parse(xhrTraduccion.responseText);
+                console.log(respuestaTraduccion)
+                // Creo una variable para almacenar los resultados de las traducciones
+                var resultadosTraducciones = '';
+        
+                // Itero sobre cada traducción y agrego los resultados a la variable
+                for (var i = 0; i < respuestaTraduccion.length; i++) {
+                    for (var j = 0; j < respuestaTraduccion[i].translations.length; j++) {
+                        resultadosTraducciones += respuestaTraduccion[i].translations[j].to + ': ' + respuestaTraduccion[i].translations[j].text + '\n';
+                    }
+                }
+                // Muestro los resultados en el área de texto con id 'results'
+                document.getElementById('results').value = resultadosTraducciones;
+            }
+        };
+       
     }
 });
-
-
-
-
-
-
-
-
-
